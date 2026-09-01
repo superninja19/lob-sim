c261af0e78e1d94a1cfd6c2a029b1e619c545181
Chris D <dawk19@hotmail.com>
2026-09-01 01:13:02 -0400

    Add Level struct and general fixes

--- include/lob/order_book.hpp
+++ include/lob/order_book.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <cstdint>
+#include <functional>
 #include <list>
 #include <map>
 #include <optional>
@@ -7,22 +9,26 @@
 
 namespace lob {
 
-using Level = std::list<LimitOrder>;
 using Price = std::int64_t;
 using OrderId = std::uint64_t;
 using Quantity = std::int64_t;
 
+struct Level{
+    std::list<LimitOrder> orders;
+    Quantity totalQuantity;
+};
+
 class OrderBook{
 public:
     void addOrder(LimitOrder order);
-    std::optional<Price> bestBid();
-    std::optional<Price> bestAsk();
-    OrderId getAndIncrementOrderId();
-    Quantity getQuantity(Price price);
-    OrderId Submit(OrderRequest orderRequest);
+    std::optional<Price> bestBid() const;
+    std::optional<Price> bestAsk() const;
+    Quantity getQuantity(Price price) const;
+    OrderId submit(OrderRequest orderRequest);
 
 private:
-    OrderId nextOrderId = 1;
+    OrderId allocateOrderId();
+    OrderId nextOrderId_ = 1;
     std::map<Price,Level> askSideMap_;
     std::map<Price,Level,std::greater<Price>> bidSideMap_;
 };
